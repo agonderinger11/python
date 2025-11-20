118d36c2d94d8cb6c139d03d5e0e57e7e278da6f
Austin Gonderinger <austin.gonderinger1@outlook.com>
2025-11-19 20:53:02 -0600

    Television commit

---
+++ television.py
@@ -0,0 +1,75 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+    
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+        
+        self.__temp = 0
+        
+    def power(self):
+        if self.__status == False:
+            self.__status = True
+        else: 
+            self.__status = False
+    
+    def mute(self):
+        if self.__status == False:
+            return
+        
+        if self.__muted == False:
+            self.__muted = True
+            self.__temp = self.__volume
+            self.__volume = 0
+        else: 
+            self.__muted = False
+    
+    def channel_up(self):
+        if self.__status == False:
+            return
+        
+        if self.__channel == self.MAX_CHANNEL:
+            self.__channel = self.MIN_CHANNEL
+        else:
+            self.__channel += 1
+
+    def channel_down(self):
+        if self.__status == False:
+            return
+        
+        if self.__channel == self.MIN_CHANNEL:
+            self.__channel = self.MAX_CHANNEL
+        else:
+            self.__channel -= 1
+    
+    def volume_up(self):
+        if self.__status == False :
+            return
+        elif self.__muted == True:
+            self.__muted = False
+            self.__volume = self.__temp
+        
+        if self.__volume == self.MAX_VOLUME:
+            self.__volume = self.MAX_VOLUME
+        else:
+            self.__volume += 1
+    
+    def volume_down(self):
+        if self.__status == False:
+            return
+        elif self.__muted == True:
+            self.__muted = False
+            self.__volume = self.__temp
+        
+        if self.__volume == self.MIN_VOLUME:
+            self.__volume = self.MIN_VOLUME
+        else:
+            self.__volume -= 1
+    
+    def __str__(self):
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
